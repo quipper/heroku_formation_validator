255d135d8e1eb37cbbe9ba38d15f778d13f48fb4
Tomo Nakano <tomo@quipper.com>
2013-12-26 14:06:39 +0000

    implemented papertrail plugin

--- lib/heroku_formation_validator/plugins/papertrail.rb
+++ lib/heroku_formation_validator/plugins/papertrail.rb
@@ -3,7 +3,10 @@ module HerokuFormationValidator
     class Papertrail
       def self.run(heroku_api, app, values)
         errors = []
-        heroku_api
+        drains = heroku_api.logdrains(app)
+        unless drains.detect{|drain| drain["url"] =~ /^syslog:\/\/logs.papertrailapp.com/}
+          errors << "not set in logdrains"
+        end
         errors
       end
     end
